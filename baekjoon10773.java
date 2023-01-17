f582fbe2f5192f993906a9adccce86867ce39a12
KimMinHo-mino <121854387+KimMinHo-mino@users.noreply.github.com>
2023-01-17 14:41:33 +0900

    Add files via upload

--- baekjoon10773.java
+++ baekjoon10773.java
@@ -1,20 +1,32 @@
 package baekjoon;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class baekjoon10773 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int K = scanner.nextInt();
-        int stack[] = new int[K];
-        int topindex = 0;
-        stack[topindex] = scanner.nextInt();
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < K-1; i++){
-            if(scanner.nextInt() != 0) {
-                topindex++;
-            } // 막혔는데 최대한 풀어가겠습니다.
+        int K = Integer.parseInt(scanner.nextLine());
+        int num = 0;
+        int result = 0;
+
+        for(int i = 0; i < K; i++){
+            int input = scanner.nextInt();
+            if(input == 0){
+                stack.pop();
+            }
+            else{
+                stack.push(input);
+            }
         }
+
+        while (stack.empty() == false){
+            num = stack.peek();
+            stack.pop();
+            result += num;
+        }
+        System.out.println(result);
     }
 }
-ㄴㄴㄴㄴ
